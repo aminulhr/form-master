741a309c6438974e9c69b1ce23870356eddb7b71
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-01-15 23:47:17 +0600

    use State and OnChange

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handelName = (e) => {
+    setName(e.target.value);
+  };
+  const handelEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handelPassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const [error, setError] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.name.value);
-    console.log(e.target.email.value);
-    console.log(e.target.number.value);
-    console.log("Form submitted");
+    if (password.length < 6) {
+      setError(alert("Password must be at least 6 characters long"));
+    } else {
+      setError("");
+      console.log(name, email, password, error);
+    }
   };
   return (
     <div>
@@ -13,21 +31,25 @@ const Form = () => {
           Fill in the Form
         </h1>
         <input
+          onChange={handelName}
           className="bg-gray-100 text-black p-4 m-4 rounded-md"
           type="text"
           name="name"
         />{" "}
         <br />
         <input
+          onChange={handelEmail}
           className="bg-gray-100 text-black p-4 m-4 rounded-md"
           type="email"
           name="email"
         />{" "}
         <br />
         <input
+          onChange={handelPassword}
           className="bg-gray-100 text-black p-4 m-4 rounded-md"
-          type="number"
-          name="number"
+          type="password"
+          name="password"
+          required
         />{" "}
         <br />
         <input
@@ -36,6 +58,11 @@ const Form = () => {
           value="Submit Form"
         />{" "}
         <br />
+        {error && (
+          <p className="text-3lx text-red-500" role="alert">
+            {error}
+          </p>
+        )}
       </form>
     </div>
   );

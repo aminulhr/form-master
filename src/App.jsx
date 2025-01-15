faace6fb887b462663b2f28397cc948a70697fbb
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-01-15 22:23:51 +0600

    setup projest

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DaisyUI from "./DaisyUI";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <DaisyUI />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DaisyUI.jsx
@@ -0,0 +1,14 @@
+const DaisyUI = () => {
+  return (
+    <div>
+      <form>
+        <input className="rounded-md m-3 p-3" type="text" /> <br />
+        <button className="bg-blue-700 rounded-md m-3 p-3 w-150 text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default DaisyUI;

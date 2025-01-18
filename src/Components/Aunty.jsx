e3c03409091384cea2d18a12d2f51600380c12e3
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-01-18 19:14:36 +0600

    Context API USE

--- src/Components/Aunty.jsx
+++ src/Components/Aunty.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import Cousin from "./Cousin";
+import { HadiaContext } from "./GrandPa";
 
 const Aunty = () => {
+  const [hadia, sethadia] = useContext(HadiaContext);
   return (
     <div>
       <h1>Aunty</h1>
@@ -8,6 +11,13 @@ const Aunty = () => {
         <Cousin name={"Rifat"}></Cousin>
         <Cousin name={"Rotna"}></Cousin>
       </section>
+      <p>Hadia: {hadia}</p>
+      <button
+        className="bg-blue-400 p-2 m-2 rounded-md"
+        onClick={() => sethadia(hadia + 500)}
+      >
+        Give More
+      </button>
     </div>
   );
 };
--- src/Components/GrandPa.jsx
+++ src/Components/GrandPa.jsx
@@ -1,20 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "./Aunty";
 import Dad from "./Dad";
 import "./grandpa.css";
 import Uncle from "./Uncle";
 export const GiftContext = createContext("Ring");
+export const HadiaContext = createContext(500);
 const GrandPa = () => {
+  const [hadia, sethadia] = useState(500);
   return (
     <div className="grandpa text-yellow-200">
       <h1 className="text-2xl text-center font-extrabold">Grandpa</h1>
-      <GiftContext.Provider value="Ring">
-        <section className="flex">
-          <Dad />
-          <Uncle />
-          <Aunty />
-        </section>
-      </GiftContext.Provider>
+      <p className="text-center">Total Hadia: {hadia}</p>
+      <HadiaContext.Provider value={[hadia, sethadia]}>
+        <GiftContext.Provider value="Ring">
+          <section className="flex">
+            <Dad />
+            <Uncle />
+            <Aunty />
+          </section>
+        </GiftContext.Provider>
+      </HadiaContext.Provider>
     </div>
   );
 };
